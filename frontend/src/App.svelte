86b4bc2ffa31ae4e72176920485ca00b5012962f
maroisa <ahmadroisul412@gmail.com>
2025-10-30 23:10:42 +0700

    add basic frontend

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,3 +1,26 @@
-<main class="bg-black">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sed harum asperiores similique vel at assumenda neque. Nobis explicabo saepe, illo, consequatur officiis delectus omnis nam atque deleniti ex accusamus!
+<main class=" bg-[#21222b] text-blue-100 p-6 flex flex-col gap-6">
+
+  <span class="font-semibold text-lg">
+    Input directory:
+  </span>
+  <button class="h-12 card">
+    <span class="text-blue-100/50">
+      Select Input Directory...
+    </span>
+  </button>
+
+  <span class="font-semibold text-lg mt-4">
+    Output directory:
+  </span>
+  <button class="h-12 card">
+    <span class="text-blue-100/50">
+      Select Output Directory...
+    </span>
+  </button> 
+  <div class="grow card bg-transparent p-4 text-blue-100/50 overflow-auto">
+    <p class="text-center">-- Log --</p>
+    <hr class="text-blue-100/10 my-2 overflow-auto">
+    <p></p>
+  </div>
+  <button class="h-12 card bg-cyan-100/60 hover:bg-cyan-100/70 active:bg-cyan-100 text-slate-800 font-bold text-lg">Compress</button>
 </main>
